2da30fd37e7ba9344bce48d8d8a83368e110243e
chiubowen <cbh@cameo.tw>
2023-02-16 12:20:28 +0800

    #!/bin/bash
    for i in {1..10}; do
        gcloud compute instances create "vm-${i}" \
            --boot-disk-size=10 \
            --boot-disk-type=pd-standard \
            --zone=us-central1-a \
            --preemptible &

--- gcloud_spot.sh
+++ gcloud_spot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for i in {1..10}; do
-    nohup gcloud compute instances create "vm-${i}" \
+    gcloud compute instances create "vm-${i}" \
         --boot-disk-size=10 \
         --boot-disk-type=pd-standard \
         --zone=us-central1-a \
